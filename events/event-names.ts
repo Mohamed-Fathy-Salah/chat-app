f736985b1d858a25d381ca08852e289c8ca1b0c3
Mohamed Fathy <mofasa.peace@gmail.com>
2022-09-09 20:33:03 +0200

    event names

---
+++ events/event-names.ts
@@ -0,0 +1,13 @@
+export enum EventNames {
+    MESSAGE = "message",
+    ADMIN_CREATED = "admin:created",
+    USER_JOINED = "user:joined",
+    ADMIN_REMOVED = "admin:removed",
+    USER_REMOVED = "user:removed",
+    FRIEND_ADDED = "friend:added",
+    FRIEND_REMOVED = "friend:removed",
+    GROUP_CREATED = "group:created",
+    GROUP_DELETED = "group:deleted",
+    GROUP_UPDATED = "group:updated",
+    USER_UPDATED = "user:updated"
+}
