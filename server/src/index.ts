5b9688d257ed93e77f058c34aceac4b1d43ac6a0
Mohamed Fathy <mofasa.peace@gmail.com>
2022-10-04 17:41:29 +0200

    fixed redis pub/sub

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,14 +3,16 @@ import { sequelize } from "./models/sequelize-wrapper";
 import { app } from "./app";
 import { Server, Socket } from "socket.io";
 import { Connection } from "./models/connection";
+import { createAdapter } from "@socket.io/redis-adapter";
 import { createClient } from "redis";
 
+const rand = Math.random();
 const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
   if (!process.env.REDIS_URL) {
-    throw new Error("REDIS_HOST must be defined");
+    throw new Error("REDIS_URL must be defined");
   }
   if (!process.env.POSTGRES_URL) {
     throw new Error("POSTGRES_URL must be defined");
@@ -18,44 +20,44 @@ const start = async () => {
 
   const PORT = "3000";
 
-  const subscriber = createClient({ url: process.env.REDIS_URL });
-
-  const publisher = createClient({ url: process.env.REDIS_URL });
-
-  subscriber.on("message", async function (channel, msg) {
-    try {
-      console.log(`received msg in channel ${channel} msg: ${msg}`);
-      if (msg.isGroup) {
-        const groupId = msg.to;
-        const users = await Connection.findAll({
-          where: { groupId },
-          attributes: { exclude: ["groupId"] },
-        });
-
-        users.forEach((user) => {
-          if (user.userId !== msg.from) {
-            console.log(user.userId.toString());
-            //@ts-ignore
-            io.to(user.userId).emit("message", msg);
-          }
-        });
-      } else {
-        io.to(msg.to).emit("message", msg);
-      }
-    } catch (e) {
-      console.error("--->", e);
-    }
-  });
-
   await sequelize.sync();
 
   const server = createServer(app);
 
   const io = new Server(server, {});
 
+  const publisher = createClient({ url: process.env.REDIS_URL });
+  const subscriber = publisher.duplicate();
+
+  await publisher.connect();
+  await subscriber.connect();
+
+  io.adapter(createAdapter(publisher, subscriber));
+
   io.on("connection", (socket: Socket) => {
-    socket.on("chatMessage", (msg) => {
-      publisher.publish("message", msg);
+    socket.on("chatMessage", async (msg) => {
+      try {
+        if (msg.isGroup) {
+          const groupId = msg.to;
+          const users = await Connection.findAll({
+            where: { groupId },
+            attributes: { exclude: ["groupId"] },
+          });
+
+          users.forEach((user) => {
+            if (user.userId !== msg.from) {
+              console.log(user.userId.toString());
+              //@ts-ignore
+              io.to(user.userId).emit("message", msg);
+            }
+          });
+        } else {
+          msg.body += rand;
+          io.to(msg.to).emit("message", msg);
+        }
+      } catch (e) {
+        console.error("--->", e);
+      }
     });
 
     socket.on("join", (room) => {
